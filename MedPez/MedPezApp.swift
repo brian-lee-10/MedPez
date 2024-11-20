c5e4647336e0cab9101abe48e6bb14b68c2f666b
brian-lee-10 <brianlee891@gmail.com>
2024-11-19 23:52:20 -0800

    Initial Commit

---
+++ MedPez/MedPezApp.swift
@@ -0,0 +1,17 @@
+//
+//  MedPezApp.swift
+//  MedPez
+//
+//  Created by Brian Lee on 11/19/24.
+//
+
+import SwiftUI
+
+@main
+struct MedPezApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
