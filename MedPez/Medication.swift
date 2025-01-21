0e740c5892a31125e52354616db695ba35b46004
Brian Lee <brianlee891@gmail.com>
2025-01-20 19:48:09 -0800

    Create Medication.swift

---
+++ MedPez/Medication.swift
@@ -0,0 +1,15 @@
+//
+//  Medication.swift
+//  MedPez
+//
+//  Created by Brian Lee on 1/14/25.
+//
+
+import Foundation
+
+struct Medication: Identifiable {
+    var id: String
+    var name: String
+    var dosage: String
+    var schedule: String
+}
