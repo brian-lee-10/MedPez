cf9f5d1159767e86ebe31decaa83c362264da5ac
Brian Lee <brianlee891@gmail.com>
2025-01-20 19:48:11 -0800

    Update MedPezApp.swift

--- MedPez/MedPezApp.swift
+++ MedPez/MedPezApp.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 import FirebaseCore
 import FirebaseAuth
+import FirebaseFirestore
 
 class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication,
