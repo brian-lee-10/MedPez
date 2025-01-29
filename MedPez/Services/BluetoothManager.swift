c5ccca484edbbe5f20201a734f826031cc73ff48
Brian Lee <brianlee891@gmail.com>
2025-01-29 14:19:21 -0800

    Added Bluetooth Functionality

--- MedPez/Services/BluetoothManager.swift
+++ MedPez/Services/BluetoothManager.swift
@@ -5,3 +5,82 @@
 //  Created by Brian Lee on 1/28/25.
 //
 
+import Foundation
+import SwiftUI
+import CoreBluetooth
+
+class BluetoothManager: NSObject, ObservableObject, CBCentralManagerDelegate, CBPeripheralDelegate {
+    var myCentral: CBCentralManager!
+    @Published var isSwitchedOn = false
+    @Published var peripherals = [Peripheral]()
+    @Published var connectedPeripheralUUID: UUID?
+    
+    override init() {
+        super.init()
+        myCentral = CBCentralManager(delegate:self, queue: nil)
+    }
+    
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        isSwitchedOn = central.state == .poweredOn
+        if isSwitchedOn {
+            startScanning()
+        } else {
+            stopScanning()
+        }
+    }
+    
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String: Any], rssi RSSI: NSNumber) {
+        let newPeripheral = Peripheral(id: peripheral.identifier, name: peripheral.name ?? "Unknown", rssi: RSSI.intValue)
+        
+        if !peripherals.contains(where: {$0.id == newPeripheral.id}) {
+            DispatchQueue.main.async {
+                self.peripherals.append(newPeripheral)
+            }
+        }
+    }
+    
+    
+    func startScanning() {
+        print("startScanning")
+        myCentral.scanForPeripherals(withServices: nil, options: nil)
+    }
+    
+    func stopScanning() {
+        print("stopScanning")
+        myCentral.stopScan()
+    }
+    
+    func connect(to peripheral: Peripheral) {
+        guard let cbPeripheral = myCentral.retrievePeripherals(withIdentifiers: [peripheral.id]).first
+        else { // Retrieve the peripheral by its identifier
+            print("Peripheral not found for connection")
+            return
+        }
+        
+        connectedPeripheralUUID = cbPeripheral.identifier
+        cbPeripheral.delegate = self // Set self as the delegate of the peripheral
+        myCentral.connect (cbPeripheral, options: nil)
+    }
+    
+    
+    func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
+        print( "Connected to \(peripheral.name ?? "Unknown")") // Print a message to the console
+        peripheral.discoverServices(nil)
+    }
+    
+    func centralManager(_ central: CBCentralManager, didFailToConnect peripheral: CBPeripheral, error: Error?) {
+        print( "Failed to connect to \(peripheral.name ?? "Unknown"): \(error?.localizedDescription ?? "No error information")") // Print a message to the console
+        if peripheral.identifier == connectedPeripheralUUID {
+            connectedPeripheralUUID = nil
+        }
+    }
+    
+    // Delegate method called when a peripheral is disconnected
+    func centralManager(_ central: CBCentralManager, didDisconnectPeripheral peripheral: CBPeripheral, error: Error?) {
+        print( "Disconnected from \(peripheral.name ?? "Unknown")") // Print a message to the console
+        if peripheral.identifier == connectedPeripheralUUID { // Check if the disconnected peripheral is the connected one
+            connectedPeripheralUUID = nil // Clear the connected peripheral JUID
+        }
+    }
+    
+}
--- MedPez/Views/BluetoothView.swift
+++ MedPez/Views/BluetoothView.swift
@@ -5,14 +5,77 @@
 //  Created by Brian Lee on 1/27/25.
 //
 
+
+import Foundation
 import SwiftUI
+import CoreBluetooth
 
 struct BluetoothView: View {
-    var body: some View {
-        Text("BluetoothView")
+    @StateObject var bluetoothManager = BluetoothManager()
+    
+    var body: some View{
+        VStack(spacing: 10){
+            Text ("Bluetooth Devices")
+                .font(.largeTitle)
+                .frame(maxWidth: .infinity, alignment: .center)
+            
+            List(bluetoothManager.peripherals) { peripheral in
+                HStack {
+                    Text(peripheral.name)
+                    Spacer ()
+                    Text(String(peripheral.rssi))
+                    Button (action: {
+                        bluetoothManager.connect (to: peripheral)
+                    }) {
+                        if bluetoothManager.connectedPeripheralUUID == peripheral.id {
+                            Text("Connected")
+                                .foregroundColor(.green)
+                        } else {
+                            Text("Connect")
+                        }
+                    }
+                }
+            }
+            .frame(height: UIScreen.main.bounds.height / 2)
+            
+            Spacer()
+            
+            Text("Status")
+                .font(.headline)
+            
+            if bluetoothManager.isSwitchedOn {
+                Text("Bluetooth is ON")
+                    .foregroundColor(.green)
+            } else {
+                Text("Bluetooth is OFF")
+                    .foregroundColor(.red)
+            }
+            
+            Spacer()
+            
+            VStack(spacing: 25) {
+                Button(action: {
+                    bluetoothManager.startScanning()
+                }) {
+                    Text ("Start Scanning")
+                }.buttonStyle(BorderedProminentButtonStyle())
+                
+                Button(action: {
+                    bluetoothManager.stopScanning()
+                }) {
+                    Text ("Stop Scanning")
+                }
+                .buttonStyle(BorderedProminentButtonStyle())
+            }
+            .padding()
+            
+            Spacer()
+        }
+        .onAppear {
+            if bluetoothManager.isSwitchedOn {
+                bluetoothManager.startScanning()
+            }
+        }
     }
 }
 
-#Preview {
-    BluetoothView()
-}
