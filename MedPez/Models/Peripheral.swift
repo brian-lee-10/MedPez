aee29371b48d2c53f4c3b4c3ec26de83b5fc8f7d
Brian Lee <brianlee891@gmail.com>
2025-01-29 14:19:44 -0800

    Create Peripheral.swift

---
+++ MedPez/Models/Peripheral.swift
@@ -0,0 +1,14 @@
+//
+//  Peripheral.swift
+//  MedPez
+//
+//  Created by Brian Lee on 1/28/25.
+//
+
+import Foundation
+
+struct Peripheral: Identifiable {
+    let id: UUID
+    let name: String
+    let rssi: Int
+}
