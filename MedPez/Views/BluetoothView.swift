5025299b64ea056f639aa80c8f8912c167f4ab5d
Brian Lee <brianlee891@gmail.com>
2025-02-03 13:27:34 -0800

    Update BluetoothView.swift

--- MedPez/Views/BluetoothView.swift
+++ MedPez/Views/BluetoothView.swift
@@ -14,67 +14,76 @@ struct BluetoothView: View {
     @StateObject var bluetoothManager = BluetoothManager()
     
     var body: some View{
-        VStack(spacing: 10){
-            Text ("Bluetooth Devices")
-                .font(.largeTitle)
-                .frame(maxWidth: .infinity, alignment: .center)
-            
-            List(bluetoothManager.peripherals) { peripheral in
-                HStack {
-                    Text(peripheral.name)
-                    Spacer ()
-                    Text(String(peripheral.rssi))
-                    Button (action: {
-                        bluetoothManager.connect (to: peripheral)
-                    }) {
-                        if bluetoothManager.connectedPeripheralUUID == peripheral.id {
-                            Text("Connected")
-                                .foregroundColor(.green)
-                        } else {
-                            Text("Connect")
+        NavigationStack {
+            VStack(spacing: 10){
+                List(bluetoothManager.peripherals.filter { $0.name != "Unknown" }) { peripheral in
+                    HStack {
+                        Text(peripheral.name)
+                        Spacer ()
+                        Text(String(peripheral.rssi))
+                        Button (action: {
+                            bluetoothManager.connect (to: peripheral)
+                        }) {
+                            if bluetoothManager.connectedPeripheralUUID == peripheral.id {
+                                Text("Connected")
+                                    .foregroundColor(.green)
+                            } else {
+                                Text("Connect")
+                            }
                         }
                     }
                 }
-            }
-            .frame(height: UIScreen.main.bounds.height / 2)
-            
-            Spacer()
-            
-            Text("Status")
-                .font(.headline)
-            
-            if bluetoothManager.isSwitchedOn {
-                Text("Bluetooth is ON")
+                .frame(height: UIScreen.main.bounds.height / 2)
+
+                if let connectedPeripheralUUID = bluetoothManager.connectedPeripheralUUID, let connectedPeripheral = bluetoothManager.peripherals.first(where: { $0.id == connectedPeripheralUUID }) {
+                    Text("Connected to \(connectedPeripheral.name)")
                     .foregroundColor(.green)
-            } else {
-                Text("Bluetooth is OFF")
+                    .padding()
+                } else {
+                    Text("Not connected to any device")
                     .foregroundColor(.red)
-            }
-            
-            Spacer()
-            
-            VStack(spacing: 25) {
-                Button(action: {
-                    bluetoothManager.startScanning()
-                }) {
-                    Text ("Start Scanning")
-                }.buttonStyle(BorderedProminentButtonStyle())
-                
-                Button(action: {
-                    bluetoothManager.stopScanning()
-                }) {
-                    Text ("Stop Scanning")
+                    .padding()
                 }
-                .buttonStyle(BorderedProminentButtonStyle())
+                
+                Spacer()
+                
+                Text("Status")
+                    .font(.headline)
+                
+                if bluetoothManager.isSwitchedOn {
+                    Text("Bluetooth is ON")
+                        .foregroundColor(.green)
+                } else {
+                    Text("Bluetooth is OFF")
+                        .foregroundColor(.red)
+                }
+                
+                Spacer()
+                
+                VStack(spacing: 25) {
+                    Button(action: {
+                        bluetoothManager.startScanning()
+                    }) {
+                        Text ("Start Scanning")
+                    }.buttonStyle(BorderedProminentButtonStyle())
+                    
+                    Button(action: {
+                        bluetoothManager.stopScanning()
+                    }) {
+                        Text ("Stop Scanning")
+                    }
+                    .buttonStyle(BorderedProminentButtonStyle())
+                }
+                .padding()
+                
+                Spacer()
             }
-            .padding()
-            
-            Spacer()
-        }
-        .onAppear {
-            if bluetoothManager.isSwitchedOn {
-                bluetoothManager.startScanning()
+            .onAppear {
+                if bluetoothManager.isSwitchedOn {
+                    bluetoothManager.startScanning()
+                }
             }
+            .navigationTitle("Bluetooth")
         }
     }
 }
