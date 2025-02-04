a4a101b4cbf733351967f507d7eab97188a7d6f5
Brian Lee <brianlee891@gmail.com>
2025-02-03 22:04:40 -0800

    Update BluetoothView.swift

--- MedPez/Views/BluetoothView.swift
+++ MedPez/Views/BluetoothView.swift
@@ -16,6 +16,25 @@ struct BluetoothView: View {
     var body: some View{
         NavigationStack {
             VStack(spacing: 10){
+                // Text("Bluetooth Devices")
+                //     .font(.title)
+                //     .padding()
+                
+                Spacer()
+                
+                HStack {
+                    Text("Status")
+                    .font(.headline)
+                
+                    if bluetoothManager.isSwitchedOn {
+                        Text("Bluetooth is ON")
+                            .foregroundColor(.green)
+                    } else {
+                        Text("Bluetooth is OFF")
+                            .foregroundColor(.red)
+                    }
+                }
+                
                 List(bluetoothManager.peripherals.filter { $0.name != "Unknown" }) { peripheral in
                     HStack {
                         Text(peripheral.name)
@@ -37,30 +56,22 @@ struct BluetoothView: View {
 
                 if let connectedPeripheralUUID = bluetoothManager.connectedPeripheralUUID, let connectedPeripheral = bluetoothManager.peripherals.first(where: { $0.id == connectedPeripheralUUID }) {
                     Text("Connected to \(connectedPeripheral.name)")
-                    .foregroundColor(.green)
-                    .padding()
+                        .foregroundColor(.green)
+                        .padding()
+                    Button(action: {
+                        bluetoothManager.disconnect()
+                    }) {
+                        Text("Disconnect")
+                            .foregroundColor(.red)
+                    }
+                    .buttonStyle(BorderedButtonStyle())
                 } else {
                     Text("Not connected to any device")
-                    .foregroundColor(.red)
-                    .padding()
-                }
-                
-                Spacer()
-                
-                Text("Status")
-                    .font(.headline)
-                
-                if bluetoothManager.isSwitchedOn {
-                    Text("Bluetooth is ON")
-                        .foregroundColor(.green)
-                } else {
-                    Text("Bluetooth is OFF")
                         .foregroundColor(.red)
+                        .padding()
                 }
                 
-                Spacer()
-                
-                VStack(spacing: 25) {
+                HStack(spacing: 25) {
                     Button(action: {
                         bluetoothManager.startScanning()
                     }) {
@@ -87,4 +98,3 @@ struct BluetoothView: View {
         }
     }
 }
-
