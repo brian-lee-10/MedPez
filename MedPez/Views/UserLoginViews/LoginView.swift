6efbfba275672924801b5a1c9c6d0606330ed85c
Brian Lee <brianlee891@gmail.com>
2025-03-06 17:23:52 -0800

    Update LoginView.swift

--- MedPez/Views/UserLoginViews/LoginView.swift
+++ MedPez/Views/UserLoginViews/LoginView.swift
@@ -12,135 +12,125 @@ struct LoginView: View {
         if isLoggedIn {
             MainTabView()
         } else {
-            ZStack {
-                // High-contrast Gradient Background
-                LinearGradient(gradient: Gradient(colors: [Color.black, Color.purple]), startPoint: .top, endPoint: .bottom)
-                    .edgesIgnoringSafeArea(.all)
-
-                VStack(spacing: 15) {
-                    Spacer()
-
-                    // Logo & Title
-                    HStack {
-                        Image(systemName: "circle.grid.3x3.fill")
-                            .resizable()
-                            .frame(width: 60, height: 60)
-                            .foregroundColor(.white)
-
-                        Text("MedPez")
-                            .font(.largeTitle)
-                            .bold()
-                            .foregroundColor(.white)
-                    }
-                    .padding(.bottom, 10)
-
-                    Text("Welcome back")
-                        .font(.title)
+            VStack(spacing: 20) {
+                Spacer()
+                HStack {
+                    Image(systemName: "pills")
+                        .resizable()
+                        .frame(width: 50, height: 50)
+                        .foregroundColor(.black)
+                    Text("MedPez")
+                        .font(.custom("OpenSans-Bold", size: 30))
                         .bold()
-                        .foregroundColor(.white)
-                        .dynamicTypeSize(.large)
-
-                    Text("Login to access your account below.")
-                        .foregroundColor(.white.opacity(0.8))
-                        .padding(.vertical, 3)
-                        .dynamicTypeSize(.medium)
-
-                    // Email Input Field
-                    VStack(alignment: .leading, spacing: 5) {
-                        Text("Email Address")
-                            .foregroundColor(.white)
-                            .fontWeight(.bold)
-
-                        TextField("Enter your email", text: $email)
-                            .keyboardType(.emailAddress)
-                            .autocapitalization(.none)
-                            .textContentType(.emailAddress)
-                            .padding()
-                            .background(Color.white.opacity(0.2))
-                            .cornerRadius(8)
-                            .foregroundColor(.white)
-                    }
-
-                    // Password Input Field
-                    VStack(alignment: .leading, spacing: 5) {
-                        Text("Password")
-                            .foregroundColor(.white)
-                            .fontWeight(.bold)
-
-                        HStack {
-                            if isPasswordVisible {
-                                TextField("Enter your password", text: $password)
-                            } else {
-                                SecureField("Enter your password", text: $password)
-                            }
-
-                            Button(action: { isPasswordVisible.toggle() }) {
-                                Image(systemName: isPasswordVisible ? "eye.slash.fill" : "eye.fill")
-                                    .foregroundColor(.white)
-                                    .background(Color.white.opacity(0.2))
-                                    .cornerRadius(5)
-                            }
-                        }
+                        .foregroundColor(.black)
+                }
+                Spacer()
+                Text("Welcome")
+                    .font(.custom("OpenSans-Bold", size: 30))
+                    .foregroundColor(.black)
+                    .dynamicTypeSize(.large)
+                Text("Login to access your account below.")
+                    .font(.custom("OpenSans-Regular", size: 17))
+                    .foregroundColor(.black.opacity(0.8))
+                    .padding(.vertical, 3)
+                    .dynamicTypeSize(.medium)
+                
+                // Email Input Field
+                VStack(alignment: .leading, spacing: 5) {
+                    Text("Email Address")
+                        .font(.custom("OpenSans-Bold", size: 17))
+                        .foregroundColor(.black)
+                    
+                    TextField("Enter your email...", text: $email)
+                        .keyboardType(.emailAddress)
+                        .autocapitalization(.none)
+                        .textContentType(.emailAddress)
                         .padding()
-                        .background(Color.white.opacity(0.2))
+                        .background(Color.white)
                         .cornerRadius(8)
-                        .foregroundColor(.white)
-                    }
-
-                    // Error Message
-                    if let errorMessage = errorMessage {
-                        Text(errorMessage)
-                            .foregroundColor(.red)
-                            .padding()
-                    }
-
-                    // Forgot Password Link
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 8)
+                                .stroke(Color.gray, lineWidth: 1) // Grey border
+                        )
+                        .foregroundColor(.black)
+                        .font(.custom("OpenSans-Regular", size: 17))
+                }
+                
+                // Password Input Field
+                VStack(alignment: .leading, spacing: 5) {
                     HStack {
+                        Text("Password")
+                            .font(.custom("OpenSans-Bold", size: 17))
+                            .foregroundColor(.black)
+                            .fontWeight(.bold)
+                        
+                        Spacer()
+                        
                         NavigationLink(destination: ForgotPasswordView()) {
                             Text("Forgot Password?")
-                                .foregroundColor(.white)
-                                .underline()
+                                .font(.custom("OpenSans-Bold", size: 17))
+                                .foregroundColor(.blue)
                         }
-
-                        Spacer()
-
-                        // Login Button
-                        Button(action: { loginUser() }) {
-                            Text("Login")
-                                .padding(.horizontal, 40)
-                                .padding(.vertical, 20)
-                                .background(Color.white)
+                    }
+                    
+                    HStack {
+                        if isPasswordVisible {
+                            TextField("Enter your password...", text: $password)
+                                .font(.custom("OpenSans-Regular", size: 17))
+                        } else {
+                            SecureField("Enter your password...", text: $password)
+                                .font(.custom("OpenSans-Regular", size: 17))
+                        }
+                        
+                        Button(action: { isPasswordVisible.toggle() }) {
+                            Image(systemName: isPasswordVisible ? "eye.slash.fill" : "eye.fill")
                                 .foregroundColor(.black)
-                                .cornerRadius(30)
+                                .background(Color.white)
+                                .cornerRadius(5)
                         }
                     }
                     .padding()
-
-                    // Sign Up Section
-                    ZStack {
-                        RoundedRectangle(cornerRadius: 10)
-                            .fill(Color.white.opacity(0.2))
-                            .frame(height: 50)
-
-                        HStack {
-                            Text("Don't have an account?")
-                                .foregroundColor(.white)
-                            NavigationLink(destination: RegisterView(isLoggedIn: $isLoggedIn)) {
-                                Text("Create →")
-                                    .foregroundColor(.yellow)
-                                    .bold()
-                                    .underline()
-                            }
-                        }
-                        .padding()
-                    }
-
-                    Spacer()
+                    .background(Color.white)
+                    .cornerRadius(8)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 8)
+                            .stroke(Color.gray, lineWidth: 1) // Grey border
+                    )
+                    .foregroundColor(.black)
                 }
-                .padding(.horizontal, 30)
+                
+                // Error Message
+                if let errorMessage = errorMessage {
+                    Text(errorMessage)
+                        .foregroundColor(.red)
+                        .padding()
+                }
+                
+                // Login Button
+                Button(action: { loginUser() }) {
+                    Text("Login")
+                        .font(.custom("OpenSans-Bold", size: 20))
+                        .frame(maxWidth: .infinity)
+                        .padding(.vertical, 20)
+                        .background(Color.purple)
+                        .foregroundColor(.white)
+                        .cornerRadius(30)
+                }
+                Spacer ()
+                
+                // Sign Up Section
+                NavigationLink(destination: RegisterView(isLoggedIn: $isLoggedIn)) {
+                    Text("Create an account")
+                        .font(.custom("OpenSans-Bold", size: 19))
+                        .foregroundColor(.blue)
+                }
+                
+                Spacer()
             }
+            .padding(.horizontal, 30)
         }
     }
+    
 
     private func loginUser() {
         Auth.auth().signIn(withEmail: email, password: password) { authResult, error in
