a9154ea5bb466bbf2558e8fe5622ff26c50d557e
Brian Lee <brianlee891@gmail.com>
2025-04-02 14:00:24 -0700

    Create AuthManager.swift

---
+++ MedPez/Services/AuthManager.swift
@@ -0,0 +1,18 @@
+//
+//  AuthManager.swift
+//  MedPez
+//
+//  Created by Brian Lee on 3/8/25.
+//
+
+import SwiftUI
+
+struct AuthManager: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AuthManager()
+}
