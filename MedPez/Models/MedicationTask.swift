d722ff8b270eeef8607e0200bf541963f16a2749
Brian Lee <brianlee891@gmail.com>
2025-04-02 13:59:54 -0700

    Update MedicationTask.swift

--- MedPez/Models/MedicationTask.swift
+++ MedPez/Models/MedicationTask.swift
@@ -6,21 +6,22 @@
 //
 import SwiftUI
 import SwiftData
+import FirebaseFirestore
 
+/// Swift Data Structure
 @Model
 class Task: Identifiable {
     var id: UUID
     var taskTitle: String
-    var creationDate: Date
+    var medicationDate: Date
     var isCompleted: Bool
     var tint: String
     var dosage: String
-    // var frequency: String
     
-    init(id: UUID = .init(), taskTitle: String, creationDate: Date = .init(), isCompleted: Bool = false, tint: String, dosage: String) {
+    init(id: UUID = .init(), taskTitle: String, medicationDate: Date = .init(), isCompleted: Bool = false, tint: String, dosage: String) {
         self.id = id
         self.taskTitle = taskTitle
-        self.creationDate = creationDate
+        self.medicationDate = medicationDate
         self.isCompleted = isCompleted
         self.tint = tint
         self.dosage = dosage
@@ -34,6 +35,31 @@ class Task: Identifiable {
         case "TaskColor 4": return .taskColor4
         case "TaskColor 5": return .taskColor5
         default: return .black
+        }
+    }
+}
+
+/// Firebase to Swift data structure
+struct MedicationTask: Identifiable {
+    var id: String  // Firestore document ID
+    var taskTitle: String
+    var medicationDate: Date
+    var tint: String
+    var dosage: String
+    var taskComplete: Bool
+    
+    // Convert Firestore document into MedicationTask struct
+    init?(id: String, data: [String: Any]) {
+        self.id = id
+        self.taskTitle = data["taskTitle"] as? String ?? "Untitled"
+        self.tint = data["tint"] as? String ?? "TaskColor 1"
+        self.dosage = data["dosage"] as? String ?? ""
+        self.taskComplete = data["taskComplete"] as? Bool ?? false
+
+        if let timestamp = data["creationDate"] as? Timestamp {
+            self.medicationDate = timestamp.dateValue()
+        } else {
+            self.medicationDate = Date()
         }
     }
 }
