1747f67ab339ab19f6b68589dcadab457b00fe82
Brian Lee <brianlee891@gmail.com>
2025-04-02 14:00:05 -0700

    Update TaskRowView.swift

--- MedPez/Views/MedicationTaskViews/TaskRowView.swift
+++ MedPez/Views/MedicationTaskViews/TaskRowView.swift
@@ -5,11 +5,15 @@
 //  Created by Brian Lee on 2/20/25.
 //
 import SwiftUI
+import FirebaseFirestore
+import FirebaseAuth
 
 struct TaskRowView: View {
     @Bindable var task: Task
     @Environment(\.modelContext) private var context
     @State private var showDeleteConfirmation = false  // State to track delete alert
+    
+    let db = Firestore.firestore()
 
     var body: some View {
         HStack(alignment: .top, spacing: 15) {
@@ -46,7 +50,7 @@ struct TaskRowView: View {
                             )
                         
                         /// Date Label
-                        Label(task.creationDate.format("hh:mm a"), systemImage: "clock")
+                        Label(task.medicationDate.format("hh:mm a"), systemImage: "clock")
                             .font(.custom("OpenSans-Bold", size: 12))
                             .foregroundStyle(.black)
                             .overlay(
@@ -99,17 +103,35 @@ struct TaskRowView: View {
         context.delete(task)
         try? context.save()
     }
+    
+//    private func deleteMedication() {
+//        guard let userId = Auth.auth().currentUser?.uid else {
+//            print("User not authenticated")
+//            return
+//        }
+//        
+//        guard let documentId = task.firestoreId else {
+//            print("No Firestore document ID found")
+//            return
+//        }
+//        
+//        /// Delete from Firestore
+//        db.collection("users").document(userId).collection("medications").document(documentId).delete { error in
+//            if let error = error {
+//                print("Error deleting from Firestore: \(error.localizedDescription)")
+//            } else {
+//                print("Successfully deleted from Firestore!")
+//            }
+//        }
+//    }
 
     var indicatorColor: Color {
         if task.isCompleted {
             return .green
         }
-        return task.creationDate.isSameHour ? .darkBlue : (task.creationDate.isPast ? .red : .black)
+        return task.medicationDate.isSameHour ? .darkBlue : (task.medicationDate.isPast ? .red : .black)
     }
 }
-
-
-
 
 #Preview {
     LogView()
