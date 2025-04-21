4f37f95e3998e64ae9f590b08c8d004e302ea811
Brian Lee <brianlee891@gmail.com>
2025-04-21 14:55:02 -0700

    Update RegisterUser.swift
    Added password requirements

--- MedPez/Views/UserLoginViews/RegisterUser.swift
+++ MedPez/Views/UserLoginViews/RegisterUser.swift
@@ -10,6 +10,7 @@ struct RegisterView: View {
     @State private var password = ""
     @State private var showAlert = false
     @State private var alertMessage = ""
+    @FocusState private var isPasswordFocused: Bool
     
     @Environment(\.presentationMode) var presentationMode  // <-- Add this
     
@@ -33,80 +34,108 @@ struct RegisterView: View {
             .background(Color.purple)
             .edgesIgnoringSafeArea(.top)
             
-            Text("Get Started")
-                .font(.custom("OpenSans-Bold", size: 30))
+            ScrollView {
+                Text("Get Started")
+                    .font(.custom("OpenSans-Bold", size: 30))
+                    .padding(.horizontal, 30)
+                
+                Text("Create your account below.")
+                    .font(.custom("OpenSans-Regular", size: 17))
+                    .padding(.vertical, 2)
+                    .padding(.horizontal, 30)
+                
+                // Name Field
+                VStack(alignment: .leading, spacing: 5) {
+                    Text("Full Name")
+                        .font(.custom("OpenSans-Bold", size: 17))
+                    TextField("Enter your full name...", text: $name)
+                        .font(.custom("OpenSans-Regular", size: 17))
+                        .padding()
+                        .cornerRadius(8)
+                        .background(Color.white)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 8)
+                                .stroke(Color.gray, lineWidth: 1) // Grey border
+                        )
+                }
                 .padding(.horizontal, 30)
-            
-            Text("Create your account below.")
-                .font(.custom("OpenSans-Regular", size: 17))
-                .padding(.vertical, 3)
+                
+                /// Email Field
+                VStack(alignment: .leading, spacing: 5) {
+                    Text("Email Address")
+                        .font(.custom("OpenSans-Bold", size: 17))
+                    TextField("Enter your email...", text: $email)
+                        .font(.custom("OpenSans-Regular", size: 17))
+                        .keyboardType(.emailAddress)
+                        .autocapitalization(.none)
+                        .padding()
+                        .cornerRadius(8)
+                        .background(Color.white)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 8)
+                                .stroke(Color.gray, lineWidth: 1) // Grey border
+                        )
+                }
                 .padding(.horizontal, 30)
-            
-            // Name Field
-            VStack(alignment: .leading, spacing: 5) {
-                Text("Full Name")
-                    .font(.custom("OpenSans-Bold", size: 17))
-                TextField("Enter your full name...", text: $name)
-                    .font(.custom("OpenSans-Regular", size: 17))
-                    .padding()
-                    .cornerRadius(8)
-                    .background(Color.white)
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 8)
-                            .stroke(Color.gray, lineWidth: 1) // Grey border
-                    )
+                
+                /// Password Field
+                VStack(alignment: .leading, spacing: 5) {
+                    Text("Password")
+                        .font(.custom("OpenSans-Bold", size: 17))
+                    /// Password Requirements List
+                    if isPasswordFocused {
+                        HStack {
+                            VStack{
+                                Label("At least 1 uppercase character", systemImage: hasUppercase ? "checkmark.circle.fill" : "xmark.circle")
+                                    .foregroundColor(hasUppercase ? .green : .red)
+                                
+                                Label("At least 1 number", systemImage: hasNumber ? "checkmark.circle.fill" : "xmark.circle")
+                                    .foregroundColor(hasNumber ? .green : .red)
+                            }
+                            .font(.custom("OpenSans-Regular", size: 12))
+                            
+                            VStack {
+                                Label("At least 1 lowercase character", systemImage: hasLowercase ? "checkmark.circle.fill" : "xmark.circle")
+                                    .foregroundColor(hasLowercase ? .green : .red)
+                                
+                                Label("Minimum 10 characters", systemImage: isLongEnough ? "checkmark.circle.fill" : "xmark.circle")
+                                    .foregroundColor(isLongEnough ? .green : .red)
+                            }
+                            .font(.custom("OpenSans-Regular", size: 12))
+                        }
+                    }
+                    
+                    SecureField("Enter your password...", text: $password)
+                        .font(.custom("OpenSans-Regular", size: 17))
+                        .padding()
+                        .cornerRadius(8)
+                        .background(Color.white)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 8)
+                                .stroke(Color.gray, lineWidth: 1)
+                        )
+                        .focused($isPasswordFocused)
+
+
+                }
+                .padding(.horizontal, 30)
+                
+                // Register Button
+                Button(action: { registerUser() }) {
+                    Text("Create Account")
+                        .font(.custom("OpenSans-Bold", size: 20))
+                        .frame(maxWidth: .infinity)
+                        .padding(.vertical, 20)
+                        .background(Color.purple)
+                        .foregroundColor(.white)
+                        .cornerRadius(30)
+                }
+                .padding(.horizontal, 30)
+                .padding(.vertical, 5)
             }
-            .padding(.horizontal, 30)
-            
-            // Email Field
-            VStack(alignment: .leading, spacing: 5) {
-                Text("Email Address")
-                    .font(.custom("OpenSans-Bold", size: 17))
-                TextField("Enter your email...", text: $email)
-                    .font(.custom("OpenSans-Regular", size: 17))
-                    .keyboardType(.emailAddress)
-                    .autocapitalization(.none)
-                    .padding()
-                    .cornerRadius(8)
-                    .background(Color.white)
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 8)
-                            .stroke(Color.gray, lineWidth: 1) // Grey border
-                    )
-            }
-            .padding(.horizontal, 30)
-            
-            // Password Field
-            VStack(alignment: .leading, spacing: 5) {
-                Text("Password")
-                    .font(.custom("OpenSans-Bold", size: 17))
-                SecureField("Enter your password...", text: $password)
-                    .font(.custom("OpenSans-Regular", size: 17))
-                    .padding()
-                    .cornerRadius(8)
-                    .background(Color.white)
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 8)
-                            .stroke(Color.gray, lineWidth: 1) // Grey border
-                    )
-            }
-            .padding(.horizontal, 30)
-            
-            // Register Button
-            Button(action: { registerUser() }) {
-                Text("Create Account")
-                    .font(.custom("OpenSans-Bold", size: 20))
-                    .frame(maxWidth: .infinity)
-                    .padding(.vertical, 20)
-                    .background(Color.purple)
-                    .foregroundColor(.white)
-                    .cornerRadius(30)
-            }
-            .padding(.horizontal, 30)
-            
             Spacer()
         }
-        .background(.BG)
+//        .background(.BG)
         .preferredColorScheme(.light)
         .navigationBarBackButtonHidden(true)
         .navigationBarItems(leading: Button(action: {
@@ -128,6 +157,7 @@ struct RegisterView: View {
         }
     }
     
+    /// Register User Function
     private func registerUser() {
         guard !name.isEmpty, !email.isEmpty, !password.isEmpty else {
             alertMessage = "All fields are required."
@@ -158,6 +188,24 @@ struct RegisterView: View {
             }
         }
     }
+    
+    /// Requiremens Functions
+    var hasUppercase: Bool {
+        password.range(of: "[A-Z]", options: .regularExpression) != nil
+    }
+
+    var hasLowercase: Bool {
+        password.range(of: "[a-z]", options: .regularExpression) != nil
+    }
+
+    var hasNumber: Bool {
+        password.range(of: "[0-9]", options: .regularExpression) != nil
+    }
+
+    var isLongEnough: Bool {
+        password.count >= 10
+    }
+
 }
 
 #Preview {
