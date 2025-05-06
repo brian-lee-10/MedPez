3b162586f21fa642b2f90f5602b17eb7e83d20af
Brian Lee <brianlee891@gmail.com>
2025-05-05 17:41:30 -0700

    Create NotificationNameExtensions.swift

---
+++ MedPez/Helpers/NotificationNameExtensions.swift
@@ -0,0 +1,12 @@
+//
+//  NotificationNameExtensions.swift
+//  MedPez
+//
+//  Created by Brian Lee on 5/5/25.
+//
+
+import Foundation
+
+extension Notification.Name {
+    static let medicationReminderFired = Notification.Name("medicationReminderFired")
+}
